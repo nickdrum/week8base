9c329fe92347834bd65d27180e7394b32ab878ac
tjLatrobe <tjdesouzadaw-test@students.latrobe.edu.au>
2025-01-23 10:12:40 +1100

    add loginout buttons and text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,84 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, View, Button, Alert, TextInput, SafeAreaView, Text } from 'react-native';
+import {Link, useLocalSearchParams} from 'expo-router';
+import { useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+
+const [loginout, setloginout] = useState('Login');
+const [created, setCreate] = useState('create');
+
+function loginA() {
+  if (loginout === 'logout')  {
+       setloginout('login');
+       debug("loginA", "login");
+  } else {
+       setloginout('logout');
+       debug("loginA", "logout");
+  }    
+}
+
+function debug(tag: String, str: String) {
+  console.log(tag, str);
+}
+
+
+function createA() {
+  if (created === 'create')  {
+      setCreate('created');
+      debug("createA", "created");
+    } else {
+      setCreate('create');
+      debug("createA","create")
+    }    
+      
+    }
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaProvider>
+    <SafeAreaView style = {styles.header}>
+          <View style = {styles.colContainer}>
+          <View style = {styles.rowContainer}>
+            <Text>
+              {loginout}
+            </Text>
+            <Text>
+              {created}
+            </Text>
+          </View>
+
+          <View style = {styles.rowContainer}>
+            <Button
+              title = {loginout}
+              onPress={()=>loginA()} >
+             </Button>
+             <Button
+              title = {created}
+              onPress={()=>createA()} >
+             </Button>
+          </View>
+       </View>
+       </SafeAreaView>
+       </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
+  header: {
+    padding: 40,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 30,
   },
+  colContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 30,
+  },
+ 
 });
